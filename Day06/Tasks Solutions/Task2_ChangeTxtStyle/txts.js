8fb8f16658e5fc9318dfecf5d00763559e8fbee1
Asim Qader <asem.adel00@gmail.com>
2023-06-10 13:06:19 +0300

    Add files via upload

---
+++ Day06/Tasks Solutions/Task2_ChangeTxtStyle/txts.js
@@ -0,0 +1,51 @@
+
+let parag = document.getElementById("PAR");
+
+//Font Family//
+function ChangeFont(fontType) {
+    parag.style.fontFamily = fontType;
+}
+
+//Text Align//
+function ChangeAlign(fontType) {
+    parag.style.textAlign = fontType;
+}
+
+//Line Height//
+function ChangeHeight(fontType) {
+    parag.style.lineHeight = fontType;
+}
+
+//Letter Spacing//
+function ChangeLSpace(fontType) {
+    parag.style.letterSpacing = fontType;
+}
+
+
+//Text Indent//
+function ChangeIndent(fontType) {
+    parag.style.textIndent = fontType;
+}
+
+//Text Transform//
+function ChangeTransform(fontType) {
+    parag.style.textTransform = fontType;
+}
+
+
+//Text Decoration//
+function ChangeDecorate(fontType) {
+    parag.style.textDecoration = fontType;
+}
+
+
+//Text Border//
+function ChangeBorder(fontType) {
+    parag.style.border = fontType;
+}
+
+
+//Text Border Color//
+function ChangeBorderColor(fontType) {
+    parag.style.borderColor = fontType;
+}
